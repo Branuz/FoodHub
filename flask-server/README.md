317b98643952110aa86fa6270b63a398572a3fd0
Jyri Poussu <jyri.poussu@gmail.com>
2022-09-11 19:39:01 +0300

    Adding required files for heroku test deployment

---
+++ flask-server/README.md
@@ -0,0 +1,10 @@
+# Getting Started with flask server
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `python server.py`
+
+Runs the server in the development mode.\
+Open [http://localhost:5000](http://localhost:5000)
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../client/build")
+CORS(app)
 
 #Users API Route
 @app.route("/users")
 def users():
     return {"users": ["User1", "User2", "User3"]}
 
+#Index API Route
+@app.route("/api", methods=["GET"])
+@cross_origin
+def index():
+    return {
+        "Testing api index"
+    }
+
+@app.route("/")
+def serve():
+    return send_from_directory(app.static_folder, "index.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
