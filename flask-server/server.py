19ae71e54b80c8d30e1d9753cb9e116deecc05de
Jyri P <jyri.poussu@gmail.com>
2022-10-24 00:12:56 +0300

    Made required changes for heroku push

--- flask-server/server.py
+++ flask-server/server.py
@@ -14,8 +14,7 @@ if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = uri
-app.config["SQLALCHEMY_DATABASE_URI"] = ("postgresql:///jpoussu")
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 db = SQLAlchemy(app)
 
